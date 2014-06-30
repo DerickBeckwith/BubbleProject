b0afa5479a87fcafdaa87ffec8bf814a28f23d23
Chuck Bolin <cbolin@sc.rr.com>
2014-06-29 21:28:02 -0400

    Toast added

--- src/com/example/bubbleproject/MainActivity.java
+++ src/com/example/bubbleproject/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +12,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Toast.makeText(getBaseContext(), "This is working", Toast.LENGTH_LONG).show();
+		
 	}
 }
